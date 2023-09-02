ad36d90ceb8202998ad6553e115872585e15f4a3
Dima Tytenko <amgpetronass@gmail.com>
2023-09-02 11:44:08 +0300

    added scripts

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const mobileNavLink = document.querySelectorAll('.mobile-nav-list-link');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -12,11 +13,16 @@
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
+
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  mobileNavLink.forEach(link => link.addEventListener('click', toggleMenu));
+  mobileMenu.addEventListener('click', e => {
+    if (e.target === mobileMenu) toggleMenu();
+  });
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
